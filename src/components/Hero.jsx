fc16c7f48fb649b3db5ec20407668bc583fd7498
KALLEM AKASH <akashgoud018@gmail.com>
2025-07-26 19:28:38 +0530

    resume link updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,7 +45,7 @@ const Hero = () => {
                 <span className="inline">Email</span>
               </a>
               <a
-                href="https://drive.google.com/file/d/1u5ZwaoQodpxPgZ9R65haG5uFrc9Mcqua/view?usp=sharing"
+                href="https://drive.google.com/file/d/1zqFVB1yLgwMvJDK7s7fuN5sjDz4OUY1f/view?usp=sharing"
                 className="flex items-center gap-1.5 sm:gap-2 hover:text-violet-600 dark:hover:text-violet-400 transition-colors duration-200 text-gray-600 dark:text-gray-400"
                 aria-label="View Resume"
                 target="_blank"
